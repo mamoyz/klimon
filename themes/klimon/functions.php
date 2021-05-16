be9c6e73a2f7394abc7989c35bf8d9a908d6d419
Mamo YZ <yzmamo@gmail.com>
2021-05-16 23:24:27 +0300

    asg

--- themes/klimon/functions.php
+++ themes/klimon/functions.php
@@ -91,3 +91,15 @@ function flavors_post_type() {
 
 }
 add_action( 'init', 'flavors_post_type', 0 );
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'General Settings',
+		'menu_title'	=> 'Settings',
+		'menu_slug' 	=> 'general-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+	
+}
--- themes/klimon/template-flavors.php
+++ themes/klimon/template-flavors.php
@@ -3,7 +3,7 @@
 get_header(); ?>
 <section class="about-hero klimon-hero products-hero">
 			<div class="hero-left yz-animate auto">
-				<h1 class="page-h1">PRODUCTS</h1>
+				<h1 class="page-h1"><?php the_title(); ?></h1>
 				  <?php wp_nav_menu( array( 'theme_location' => 'flavors-menu' , 'container'=> false, 'menu_class'=>'products-menu') ); ?>
 			</div>
 			<div class="hero-right">
@@ -31,10 +31,11 @@ get_header(); ?>
 							<div class="swiper-wrapper">
 								<?php
 								$flavors_carousel = get_field('flavors_carousel');
+								print_r($flavors_carousel);
 								 foreach ($flavors_carousel as $item) {
 									?>
 								<div class="swiper-slide">
-									<a href="#"><img src="<?php echo $item; ?>" alt="" /></a>
+									<a href="<?php echo the_permalink($item['carousel_product'][0]); ?>"><img src="<?php echo $item['carousel_product_image']; ?>" alt="" /></a>
 								</div>
 									<?php
 								 } 
@@ -67,7 +68,7 @@ get_header(); ?>
 								 foreach ($flavors_carousel as $item) {
 									?>
 								<div class="swiper-slide">
-									<a href="#"><img src="<?php echo $item; ?>" alt="" /></a>
+									<a href="<?php echo the_permalink($item['carousel_product'][0]); ?>"><img src="<?php echo $item['carousel_product_image']; ?>" alt="" /></a>
 								</div>
 									<?php
 								 } 
@@ -84,117 +85,88 @@ get_header(); ?>
 				</div>
 			</div>
 		</section>
-		<section class="parallax-bg parallax home-parallax-bg products-parallax-bg hidden-mobile slide-in" style="background-image: url('assets/img/products-parallax.jpg')"></section>
-		<section class="parallax-bg parallax home-parallax-bg products-parallax-bg mobile-only" style="background-image: url('assets/img/products-parallax-mobile.jpg')"></section>
+		<?php
+		 $flavors_parallax_image = get_field('flavors_parallax_image') ;
+		?>
+		<section class="parallax-bg parallax home-parallax-bg products-parallax-bg hidden-mobile slide-in" style="background-image: url('<?php echo $flavors_parallax_image['home_parallax_image']; ?>')"></section>
+		<section class="parallax-bg parallax home-parallax-bg products-parallax-bg mobile-only" style="background-image: url('<?php echo $flavors_parallax_image['home_parallax_image_mobile']; ?>')"></section>
 		<section class="product-flavors">
 			<div class="flavors-list-top">
-				<img class="logo" src="assets/img/logo-black.png" alt="" />
+				<img class="logo" src="<?php echo get_template_directory_uri();?>/assets/img/logo-black.png" alt="" />
 			</div>
 			<div class="flavors-list">
-				<div class="flavors-list-item yz-animate">
-					<div class="klimon-decor product-decor white">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
+				<?php
+// WP_Query arguments
+$args = array(
+	'post_type'              => array( 'flavors' ),
+);
+
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+if ( $query->have_posts() ) {
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		$index =  $query->current_post + 1;
+		$flavor_hero = get_field('flavor_hero',get_the_ID());
+
+		?>
+		<div class="flavors-list-item yz-animate">
+					<?php
+					if($index==1){
+?>
+<div class="klimon-decor product-decor white">
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/decor-black-inverse.png" alt="" />
 					</div>
 					<div class="klimon-decor product-decor black">
-						<img src="assets/img/decor-black.png" alt="" />
-					</div>
-					<div class="item-img">
-						<img class="bg" src="assets/img/products/p1.jpg" alt="" />
-						<div>
-							<img src="assets/img/products/p1.jpg" alt="" />
-						</div>
-					</div>
-					<div class="item-content">
-						<h3 style="color: #c16f2c">CARAMEL BRÛLÉE</h3>
-						<p>SALTED CARAMEL FLAVORED FROZEN DESSERT WITH CHOCOLATE COOKIE CRUMBLES & CRÈME BRûLéE FLAVORED SWIRL</p>
-						<a href="#">
-							<span>View More</span>
-							<img src="assets/img/view-more.svg" alt="" />
-						</a>
-					</div>
-				</div>
-				<div class="flavors-list-item yz-animate">
-					<!-- <div class="klimon-decor product-decor white">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
-					</div> -->
-					<div class="item-img">
-						<img class="bg" src="assets/img/products/p2.jpg" alt="" />
-						<div>
-							<img src="assets/img/products/p2.jpg" alt="" />
-						</div>
-					</div>
-					<div class="item-content">
-						<h3 style="color: #900000">CHERRY BOMB</h3>
-						<p>CHERRY FLAVORED FROZEN DESSERT WITH REAL BORDEAUX CHERRY CHUNKS & CHERRY SWIRL</p>
-						<a href="#">
-							<span>View More</span>
-							<img src="assets/img/view-more.svg" alt="" />
-						</a>
-					</div>
-				</div>
-				<div class="flavors-list-item yz-animate">
-					<!-- <div class="klimon-decor product-decor white">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
-					</div> -->
-					<div class="item-img">
-						<img class="bg" src="assets/img/products/p3.jpg" alt="" />
-						<div>
-							<img src="assets/img/products/p3.jpg" alt="" />
-						</div>
-					</div>
-					<div class="item-content">
-						<h3 style="color: #6fc59e">MINT CONDITION</h3>
-						<p>MINT FLAVORED FROZEN DESSERT WITH MINI DARK CHOCOLATE CHIPS & FUDGE SWIRL</p>
-						<a href="#">
-							<span>View More</span>
-							<img src="assets/img/view-more.svg" alt="" />
-						</a>
-					</div>
-				</div>
-				<div class="flavors-list-item yz-animate">
-					<!-- <div class="klimon-decor product-decor white">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
-					</div> -->
-					<div class="klimon-decor product-decor">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/decor-black.png" alt="" />
+					</div> 
+					<?php
+					}
+					if($index==4){
+						?>
+	<div class="klimon-decor product-decor">
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/decor-black-inverse.png" alt="" />
 					</div>
 					<div class="klimon-decor product-decor">
-						<img src="assets/img/decor-black-2.png" alt="" />
+						<img src="<?php echo get_template_directory_uri();?>/assets/img/decor-black-2.png" alt="" />
 					</div>
+						<?php
+						
+					}
+?>
+				
 					<div class="item-img">
-						<img class="bg" src="assets/img/products/p4.jpg" alt="" />
+						<img class="bg" src="<?php echo $flavor_hero['about_hero_image'];  ?>" alt="" />
 						<div>
-							<img src="assets/img/products/p4.jpg" alt="" />
+							<img src="<?php echo $flavor_hero['about_hero_image'];  ?>" alt="" />
 						</div>
 					</div>
 					<div class="item-content">
-						<h3 style="color: #754d26">MORNING BREW</h3>
-						<p>COFFEE FLAVORED FROZEN DESSERT WITH REAL ALMOND SLICES & FUDGE SWIRL</p>
-						<a href="#">
+						<h3 style="color: <?php the_field('flavor_color',get_the_ID()); ?>"><?php
+						 the_title(); 
+						?></h3>
+						<p><?php the_field('flavor_short_description',get_the_ID()); ?></p>
+						<a href="<?php
+						 the_permalink(); 
+						?>">
 							<span>View More</span>
-							<img src="assets/img/view-more.svg" alt="" />
+							<img src="<?php echo get_template_directory_uri();?>/assets/img/view-more.svg" alt="" />
 						</a>
 					</div>
 				</div>
-				<div class="flavors-list-item yz-animate">
-					<!-- <div class="klimon-decor product-decor white">
-						<img src="assets/img/decor-black-inverse.png" alt="" />
-					</div> -->
-					<div class="item-img">
-						<img class="bg" src="assets/img/products/p5.jpg" alt="" />
-						<div>
-							<img src="assets/img/products/p5.jpg" alt="" />
-						</div>
-					</div>
-					<div class="item-content">
-						<h3 style="color: #fead00">SUNRISE BANG</h3>
-						<p>CREAMY ORANGE FLAVORED FROZEN DESSERT WITH CREAM FLAVORED SWIRL</p>
-						<a href="#">
-							<span>View More</span>
-							<img src="assets/img/view-more.svg" alt="" />
-						</a>
-					</div>
-				</div>
+		<?php
+	}
+} else {
+	// no posts found
+}
+
+// Restore original Post Data
+wp_reset_postdata();				  
+				?>
+	
+			
 			</div>
 		</section>
 		<section class="products-no-dairy">
