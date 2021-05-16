cc1e2e329ebab077fdebc847795284d4480f39b3
Mamo YZ <yzmamo@gmail.com>
2021-05-16 23:27:20 +0300

    asg

--- themes/klimon/functions.php
+++ themes/klimon/functions.php
@@ -96,7 +96,7 @@ if( function_exists('acf_add_options_page') ) {
 	
 	acf_add_options_page(array(
 		'page_title' 	=> 'General Settings',
-		'menu_title'	=> 'Settings',
+		'menu_title'	=> 'General Settings',
 		'menu_slug' 	=> 'general-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
